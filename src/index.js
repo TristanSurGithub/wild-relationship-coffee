6be77ba69581337fddf10104460674e96a08bc52
Tristan Defachel <defachel.tristan@gmail.com>
2021-05-24 14:52:50 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// JS //
+import './assets/js/app.js'
+
+// SCSS //
+import css from './assets/sass/app.scss';
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./public/build"),
+        filename: "./bundle.js",
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.sc|ass$/,
+                use: [
+                    {loader: MiniCssExtractPlugin.loader},
+                    {loader: "css-loader"},
+                    {loader: "sass-loader"}
+                ]
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[name].[hash].[ext]'
+                }
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '/images/[name].[ext]',
+                },
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                { from: "src/assets/images", to: "images" },
+            ],
+        }),
+    ]
+};
